5f5cc09c4f34568143fd4247272bd8803d1d2d8a
mrvco-hash <marcandykouadio@gmail.com>
2024-10-07 12:08:34 +0000

    Added the base of the JS code for the UI.

--- 03_rockPaperScisor/script.js
+++ 03_rockPaperScisor/script.js
@@ -1,17 +1,18 @@
 let humanScore = 0;
 let computerScore = 0;
+let divResults = document.querySelector('.results')
 
 
 function getComputerChoice() {
-    const possibleChoices = ["rock", "paper", "scissor"];
+    const possibleChoices = ["rock", "paper", "scisor"];
     return (possibleChoices[Math.floor(Math.random() * possibleChoices.length)]);
 } 
 
 function getHumanChoice() {
     let humanChoice;
     while (true) {
-        humanChoice = prompt("Make your choice : rock paper or scissor.").toLowerCase();
-        if ((humanChoice == "rock") || (humanChoice == "paper") || (humanChoice == "scissor")) {
+        humanChoice = prompt("Make your choice : rock paper or scisor.").toLowerCase();
+        if ((humanChoice == "rock") || (humanChoice == "paper") || (humanChoice == "scisor")) {
             break;
         }
     }
@@ -21,41 +22,44 @@ function getHumanChoice() {
 
 function playRound(computerChoice, humanChoice) {
 
-    function win(a, b) {
+    function win(computer, human) {
         humanScore++
-        console.log("You win, the " + b + " beats the " + a);
-        console.log("Human score : ", humanScore);
-        console.log("Computer score : ", computerScore);
+        divResults.innerHTML =
+        `<p> You win, the ${human} beats the ${computer} </p>
+        <p> Human score : ${humanScore} </p>
+        <p> Computer score : ${computerScore} </p>`;
     }
-    function lose(a, b) {
+    function lose(computer, human) {
         computerScore++
-        console.log("You lose, the " + a + " beats the " + b);
-        console.log("Human score : ", humanScore);
-        console.log("Computer score : ", computerScore);
+        divResults.innerHTML = 
+        `<p> You lose, the ${computer} beats the ${human} </p>
+        <p> Human score : ${humanScore} </p>
+        <p> Computer score : ${computerScore} </p>`
     }
 
     if (humanChoice === computerChoice) {
-        console.log("it's a tie !");
-        console.log("Human score : ", humanScore);
-        console.log("Computer score : ", computerScore);
+        divResults.innerHTML = 
+        `<p> it's a tie !. Your choice : ${humanChoice}. Computer choice : ${computerChoice}. </p>
+        <p> Human score : ${humanScore} </p>
+        <p> Computer score : ${computerScore} </p>`
     }
 
     switch (humanChoice) {
         case "rock" :
             if (computerChoice == "paper") {
                 return lose(computerChoice, humanChoice)
-            } else if (computerChoice == "scissor") {
+            } else if (computerChoice == "scisor") {
                 return win(computerChoice, humanChoice)
             }
             break;
         case "paper" :
             if (computerChoice == "rock") {
                 return win(computerChoice, humanChoice)
-            } else if (computerChoice == "scissor") {
+            } else if (computerChoice == "scisor") {
                 return lose(computerChoice, humanChoice)
             }
             break;
-        case "scissor" :
+        case "scisor" :
             if (computerChoice == "paper") {
                 return win(computerChoice, humanChoice)
             } else if (computerChoice == "rock") {
@@ -65,12 +69,55 @@ function playRound(computerChoice, humanChoice) {
     }
 }
 
-function playGame() {
-    i = 0
-    while (i < 5) {
-        playRound(getComputerChoice(), getHumanChoice())
-        i++
+let rockButton = document.querySelector('#rock');
+let paperButton = document.querySelector('#paper');
+let scisorButton = document.querySelector('#scisor');
+
+rockButton.addEventListener('click', () => {
+    let playerSelection = 'rock';
+    playRound(getComputerChoice(), playerSelection);
+    function defineWinner(computer, human) {
+    if (computer == 5) {
+        divResults.innerHTML = `<p>Computer Win </p>`
+    } else if (human == 5) {
+        divResults.innerHTML = `<p>human Win </p>`
     }
 }
+    defineWinner(computerScore, humanScore)
+})
 
-playGame()
+paperButton.addEventListener('click', () => {
+    let playerSelection = 'paper';
+    playRound(getComputerChoice(), playerSelection);
+    function defineWinner(computer, human) {
+    if (computer == 5) {
+        divResults.innerHTML = `<p>Computer Win </p>`
+    } else if (human == 5) {
+        divResults.innerHTML = `<p>human Win </p>`
+    }
+}
+    defineWinner(computerScore, humanScore)
+})
+
+scisorButton.addEventListener('click', () => {
+    let playerSelection = 'scisor';
+    playRound(getComputerChoice(), playerSelection);
+    function defineWinner(computer, human) {
+    if (computer == 5) {
+        divResults.innerHTML = `<p>Computer Win </p>`
+    } else if (human == 5) {
+        divResults.innerHTML = `<p>human Win </p>`
+    }
+}
+    defineWinner(computerScore, humanScore)
+})
+
+// function playGame() {
+//     i = 0
+//     while (i < 5) {
+//         playRound(getComputerChoice(), getHumanChoice())
+//         i++
+//     }
+// }
+
+// playGame()
